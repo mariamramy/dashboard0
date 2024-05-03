49adcf17f48f3805e558118c49d85af9846c6d85
Mariam Ramy <166525849+mariamramy@users.noreply.github.com>
2024-05-04 02:36:16 +0300

    Add files via upload

---
+++ dashnoardd/src/main/java/com/example/dashnoardd/CardController.java
@@ -0,0 +1,4 @@
+package com.example.dashnoardd;
+
+public class CardController {
+}
--- dashnoardd/src/main/java/com/example/dashnoardd/HelloController.java
+++ dashnoardd/src/main/java/com/example/dashnoardd/HelloController.java
@@ -2,8 +2,19 @@ package com.example.dashnoardd;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.Initializable ;
+import javafx.scene.layout.HBox;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class HelloController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private HBox cardLayout;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
